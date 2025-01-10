c67d5de6e754830e43133b3716184de565c64136
NavyDevilDoc <79477977+NavyDevilDoc@users.noreply.github.com>
2025-01-10 14:09:02 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,28 +11,40 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 file_path = r"C:\Users\docsp\Desktop\AI_ML_Folder\Python_Practice_Folder\Natural_Language_Processing\RAG_Model\file_variables.env"
 load_environment_variables(file_path)
 
-# Flag to choose between manual input and automatic folder search. Can be True for manual upload or False for folder upload
-use_manual_input = True
-pdf_folder_path = os.getenv("PDF_FOLDER_PATH")
-doc_input = get_document_input(use_manual_input, pdf_folder_path)
+"""
+Available Ollama LLMs:
+    llama3.2:latest
+    llama3.2-vision:latest
+    llama3.3:70b-instruct-q2_K
+    codestral:latest
+    codellama:13b
+    granite3.1-dense:8b
+        embedding_type = sentence_transformer
+        embedding_model = ibm-granite/granite-embedding-30(or 125)m-english
+    granite-code:20b
+    granite-code:8b
+"""
 
 # Initialize driver with all required parameters
 driver = Driver(
+    # Mode Selection
     mode='llm',
     env_path=os.getenv("ENV_PATH"), 
     json_path=os.getenv("JSON_PATH"),
-    llm_type='gpt', 
-    embedding_type='gpt', 
-    llm_model='gpt-4o', 
-    embedding_model='text-embedding-3-large', 
+    # Language and Embedding Model Selection
+    llm_type='ollama',
+    embedding_type='sentence_transformer',
+    llm_model='granite3.1-dense:8b',
+    embedding_model='ibm-granite/granite-embedding-125m-english',
+    # RAG-specific Parameters
+    datastore_name='edqp-datastore-granite-30m',
     debug_mode=False, 
-    doc_input=doc_input,
-    doc_name='arwg-index', 
-    chunking_method='SEMANTIC', 
-    storage_type='PINECONE_EXISTING',
-    template_name='detailed', 
+    doc_name='test-index', 
+    chunking_method='PAGE', 
+    storage_type='LOCAL_STORAGE',
+    template_name='assistant', 
     use_ground_truth=False,
-    process_questions=True,
+    process_questions=False,
 )
 
 # Handle modes
